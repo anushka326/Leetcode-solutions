3c8cc9ec54d38fdac156cbabb1f4971f3cd01722
Anushka <anushkanpise@gmail.com>
2026-08-16 23:44:55 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.3%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int value=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            value=value^nums[i];
+        }
+        return value;
+    }
+};
