7ea5e7e73b46c22fa420ae9db2d767b7dc53d8d8
Anushka <anushkanpise@gmail.com>
2026-08-21 01:12:17 +0530

    Time: 3 ms (23.36%), Space: 30.4 MB (67.29%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
@@ -4,8 +4,8 @@ public:
     {
         int n=nums.size();
         vector<int> ans(2*n);
+        
         vector<int> rev(n);
-
         for(int i=0, j=n-1; i<n; i++, j--)
         {
             ans[i]=nums[i];
@@ -13,9 +13,18 @@ public:
         }
 
         for(int i=0, j=n; i<n; i++, j++)
-        {
             ans[j]=rev[i];
-        }
+ 
+ 
+        //===== optimal solution without taking a rev array we can directly use ====
+        // for(int i=0; i<n; i++)
+        // {
+        //     ans[i]=nums[i];
+        //     ans[2*n-1-i]= nums[i];
+        // }
+
+
         return ans;
+
     }
 };
