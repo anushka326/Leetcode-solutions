b427ece2552c7c313cd859672419c9f3bf0d7126
Anushka <anushkanpise@gmail.com>
2026-08-21 01:37:27 +0530

    Time: 0 ms (100%), Space: 263.5 MB (5.36%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,12 +3,18 @@ class Solution
 public:
     void rotate(vector<int>& nums, int k) 
     {
-        vector<int> ans(nums.size());
-        for(int i=0; i<nums.size(); i++)
-            ans[(i+k)%nums.size()]=nums[i];
+        // vector<int> ans(nums.size());
+        // for(int i=0; i<nums.size(); i++)
+        //     ans[(i+k)%nums.size()]=nums[i];
 
-        // copy into original one
-        nums=ans;
+        // // copy into original one
+        // nums=ans;
+
+        // optimal one 
+        k=k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
 
     }
 };
