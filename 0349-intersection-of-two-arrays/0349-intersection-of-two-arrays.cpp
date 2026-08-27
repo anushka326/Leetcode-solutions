dc266b60e2353faf659f5f2aebb994dd98c91285
Anushka <anushkanpise@gmail.com>
2026-08-28 02:02:18 +0530

    Time: 6 ms (11.54%), Space: 14.2 MB (57.03%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,3 +1,36 @@
+// Using VECTOR ONLY-BRUTE FORCE
+
+// class Solution 
+// {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+//     {
+//         int n=nums1.size();
+//         int m=nums2.size();
+//         int target;
+//         vector<int> arr;
+
+//         for(int i=0; i<n; i++ )
+//         {
+//             for(int j=0; j<m; j++)
+//             {
+//                 if(nums1[i] == nums2[j] )
+//                 {
+//                     target=nums1[i];
+//                     if(find(arr.begin(), arr.end(), target) != arr.end())
+//                         continue;
+//                     else 
+//                         arr.push_back(nums1[i]);
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// };
+
+
+// USING SET TO STORE UNIQUE ELEMENTS-BRUTE FORCE
+
 class Solution 
 {
 public:
@@ -5,8 +38,8 @@ public:
     {
         int n=nums1.size();
         int m=nums2.size();
-        int target;
-        vector<int> arr;
+        set<int> mySet;
+        
 
         for(int i=0; i<n; i++ )
         {
@@ -14,14 +47,15 @@ public:
             {
                 if(nums1[i] == nums2[j] )
                 {
-                    target=nums1[i];
-                    if(find(arr.begin(), arr.end(), target) != arr.end())
-                        continue;
-                    else 
-                        arr.push_back(nums1[i]);
+                   mySet.insert(nums1[i]);
                 }
+                else
+                    continue;
             }
         }
+        
+        vector<int> arr(mySet.begin(), mySet.end());
+        
         return arr;
     }
 };
