0730ba797b4176be6fa42e2379f8952d61b6b7ea
Anushka <anushkanpise@gmail.com>
2026-08-31 02:49:05 +0530

    Time: 3 ms (2.21%), Space: 11.5 MB (79.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,18 +3,35 @@ class Solution
 public:
     void sortColors(vector<int>& nums) 
     {
+        int count0=0, count1=0, count2=0;
+        
         for(int i=0; i<nums.size(); i++)
         {
-            for(int j=i+1; j<nums.size(); j++)
-            {
-                if(nums[i] >= nums[j])
-                {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
-            }
+            if(nums[i]==0)
+                count0++;
+                
+            else if(nums[i]==1)
+                count1++;
+            
+            else
+                count2++;
         }
+        
+        for(int i=0; i<count0; i++)
+        {
+            nums[i]=0;
+        }
+        
+        for(int i=count0; i<count0+count1; i++)
+        {
+            nums[i]=1;
+        }
+        
+        for(int i=count0+count1; i<count0+count1+count2; i++)
+        {
+            nums[i]=2;
+        }
+        
         for(int j=0; j<nums.size(); j++)
         {
             cout<<nums[j]<<", ";
